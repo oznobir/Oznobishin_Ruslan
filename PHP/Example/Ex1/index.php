2bfb1a39fe349341f6ace1d6d934ee4b79af43da
Oznobishin_Ruslan <oznobir@gmail.com>
2024-01-30 01:44:09 +0300

    add new Ex1 - include

---
+++ PHP/Example/Ex1/index.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','on');
+
+include 'pages/page1.php';
+echo '<br>';
+
+include 'pages/page2.php';
+echo '<br>';
+
+include 'pages/page3.php';
+echo '<br>';
