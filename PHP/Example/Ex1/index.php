7ff9c9b5fd551620e486aa342cd96e9e0f7659ad
Oznobishin_Ruslan <oznobir@gmail.com>
2024-01-30 13:17:32 +0300

    modified index.php

--- PHP/Example/Ex1/index.php
+++ PHP/Example/Ex1/index.php
@@ -1,3 +1,7 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,9 +22,6 @@
     <main>
         text-contents !
         <?php
-        error_reporting(E_ALL);
-        ini_set('display_errors', 'on');
-
         // Только file exists. Без проверки $_GET['page']
         $page = $_GET['page'];
         $path = "pages/page$page.php";
