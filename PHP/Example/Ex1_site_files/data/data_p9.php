1eb3a0fef81bfe7abe8c059842a9d9af1a39156d
Oznobishin_Ruslan <oznobir@gmail.com>
2024-02-13 05:05:24 +0300

    test menu

---
+++ PHP/Example/Ex1_site_files/data/data_p9.php
@@ -0,0 +1,6 @@
+<?php
+return
+    [
+        ['name' => null, 'type' => 'label', 'default' => 'Введите в переменную $a значение'],
+        ['name' => 'a', 'type' => 'text', 'default' => '15'],
+    ];
