62acf18f805bddb8b1ae97901fcb3a2866f1805e
Oznobishin_Ruslan <oznobir@gmail.com>
2024-02-24 23:53:40 +0300

    added data

--- PHP/Example/Ex1_site_files/data/data_p14.php
+++ PHP/Example/Ex1_site_files/data/data_p14.php
@@ -2,13 +2,15 @@
 return
     [
         'content1' => [
+            ['name' => null, 'type' => 'label', 'default' => 'Внимание! В htmlspecialchars для этого примера установлен flag: ENT_NOQUOTES'],
+            ['name' => 'htmlspecialchars', 'type' => 'security', 'default' => 'ENT_NOQUOTES'],
             ['name' => null, 'type' => 'label', 'default' => 'Введите test1 и test2'],
-            ['name' => 'test1', 'type' => 'text', 'default' => "echo = 'code ';"],
-            ['name' => 'test2', 'type' => 'text', 'default' => "eval(\$test1); echo '1234';"],
+            ['name' => 'test1', 'type' => 'text', 'default' => "\$id = 100;"],
+            ['name' => 'test2', 'type' => 'text', 'default' => "eval(\$test1); echo \$id;"],
         ],
         'content2' => [
             ['name' => 'index.php', 'type' => 'php', 'path' => 'index.php'],
-            ['name' => 'test2.php', 'type' => 'php', 'path' => 'test2.php'],
-            ['name' => 'test1.php', 'type' => 'php', 'path' => 'test1.php'],
+            ['name' => 'test2.php', 'type' => 'php', 'path' => 'test/test2.php'],
+            ['name' => 'test1.php', 'type' => 'php', 'path' => 'test/test1.php'],
         ],
     ];
--- PHP/Example/Ex1_site_files/pages/p14/index.php
+++ PHP/Example/Ex1_site_files/pages/p14/index.php
@@ -1,7 +1,4 @@
 <?php
-//$test1 = file_get_contents('test1.php');
-//$test2 = file_get_contents('test2.php');
-// Пока не работает через форму - htmlspecialchars
-$test1 = 'echo "code ";';
-$test2 = 'eval($test1); echo "1234 ";';
+//$test1 = file_get_contents('test/test1.php');
+//$test2 = file_get_contents('test/test2.php');
 eval($test2);
