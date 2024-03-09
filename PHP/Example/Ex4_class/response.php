46afcde44d20285a5805965f1be000931a1ed537
Oznobishin_Ruslan <oznobir@gmail.com>
2024-03-09 16:49:55 +0300

    ++new added with classes Ex4_class

--- PHP/Example/Ex4_class/response.php
+++ PHP/Example/Ex4_class/response.php
@@ -1,21 +1,12 @@
 <?php
-
 header("Content-type: text/plain; charset=UTF-8");
-
-if (isset($_POST['htmlspecialchars']) && $_POST['htmlspecialchars'] == 'ENT_NOQUOTES') {
-    $flagENT = ENT_NOQUOTES;
-} else $flagENT = ENT_QUOTES;
 foreach ($_POST as $key => $item) {
-    if ($key != 'htmlspecialchars') {
         if ($item != '') {
             $w = $key;
-//        $$w = htmlspecialchars($_POST[$w], ENT_QUOTES, 'UTF-8');
-            $$w = htmlspecialchars($_POST[$w], $flagENT, 'UTF-8');
+            $$w = htmlspecialchars($_POST[$w], ENT_QUOTES, 'UTF-8');
         } else {
             echo 'Нет введенных данных';
             exit();
         }
-    }
 }
 include $_FILES['php0']['tmp_name'];
-//include "../$path";
