5b75c4e5096a5ffccddc434abfa3a5ba25e5c0ed
Oznobishin_Ruslan <oznobir@gmail.com>
2024-03-31 13:21:06 +0300

    modified cart

--- PHP/Cart/Cart1/project/controllers/CartController.php
+++ PHP/Cart/Cart1/project/controllers/CartController.php
@@ -8,27 +8,35 @@ use Project\Models\CategoriesModel;
 
 /**
  * Контроллер для работы с корзиной
- *  /cart/id
  */
 class CartController extends Controller
 {
+    /** Страница с продуктами в корзине
+     *
+     * @return void страница с продуктами
+     */
     public function index(): void
     {
 
         if (!empty($_SESSION['cart']))
             $this->data['products'] = (new ProductsModel())->getProductsFromArray($_SESSION['cart']);
         $this->data['menu'] = (new CategoriesModel())->getCategoriesWithChild();
-        $this->data['title'] = '';
-        $this->data['description'] = '';
+        $this->data['title'] = 'Корзина товаров';
+        $this->data['description'] = 'Корзина товаров Гипермаркет myshop.by';
         echo $this->render('project/views/default/shopCartView.php');
     }
 
+    /** fetch
+     * Добавление в корзину (сессию) id продукта
+     * /cart/add/id/
+     * @return void json - success, количество в корзине или "Пусто"
+     */
     public function add(): void
     {
         $itemId = intval($this->parameters['id']);
         if (!$itemId) exit();
         $jsData = [];
-        if (isset($_SESSION['cart']) && !in_array($itemId, $_SESSION['cart'])) {
+        if (!empty($_SESSION['cart']) && !in_array($itemId, $_SESSION['cart'])) {
             $_SESSION['cart'][] = $itemId;
             if (count($_SESSION['cart'])) {
                 $jsData['countItems'] = count($_SESSION['cart']);
@@ -41,7 +49,11 @@ class CartController extends Controller
         }
         echo json_encode($jsData);
     }
-
+    /** fetch
+     * Удаление из корзины (сессии) id продукта
+     * /cart/remove/id/
+     * @return void json - success, количество в корзине или "Пусто"
+     */
     public function remove(): void
     {
         $itemId = intval($this->parameters['id']);
